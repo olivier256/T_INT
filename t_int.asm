351d335dffaf9b092174b7d067d4b00273e824bb
olivier256 <60171288+olivier256@users.noreply.github.com>
2022-11-17 12:07:34 +0100

    Add files via upload

--- t_int.asm
+++ t_int.asm
@@ -1,6 +1,6 @@
 ; I got 99 problems but ASM ain't one
 
-ROM_SIZE	equ	$10000
+ROM_SIZE	equ	65536
 GFX_CTRL	equ	$C00004
 GFX_DATA	equ	$C00000
 HV_COUNTER	equ	$C00008
@@ -8,36 +8,33 @@ CTRL1		equ	$A10009
 DATA1		equ	$A10003
 Z80_BUSREQ	equ	$A11100
 Z80_RESET	equ	$A11200
-tTimer		equ	$FF0000
-freq_index	equ	$FF0002
-freq_index2	equ	$FF0003
-clearHInt	equ	$FF0004
-clearVInt	equ	$FF0005
-initialSP	equ	$FFFE00
+muteHInt		equ	$FF0300
+INDEX		equ	$FF0301
+hCnt		equ	$FF0304
+vCnt		equ	$FF0306
+initialSP	equ	$1000000
 initialUSP	equ	initialSP-$100
-H_INT_RAM	equ	$FFFE00
-V_INT_RAM	equ	$FFFF00
 
 
 	DC.L	initialSP
-	DC.L	init
+	DC.L	START
 	DC.L	INT, INT, INT, INT, INT, INT
-	dc.l	INT, INT, INT, INT, INT, INT, INT, INT
-	dc.l	INT, INT, INT, INT, INT, INT, INT, INT
-	dc.l	INT, INT, INT, INT, H_INT, INT, V_INT, INT
-	dc.l	INT, INT, INT, INT, INT, INT, INT, INT
-	dc.l	INT, INT, INT, INT, INT, INT, INT, INT
-	dc.l	initialUSP			; Exception Vector #48
-	dc.l	0,0,0,0,0,0,0
-	dc.l	0,0,0,0,0,0,0,0
+	DC.L	INT, INT, INT, INT, INT, INT, INT, INT
+	DC.L	INT, INT, INT, INT, INT, INT, INT, INT
+	DC.L	INT, INT, INT, INT, H_INT, INT, V_INT, INT
+	DC.L	INT, INT, INT, INT, INT, INT, INT, INT
+	DC.L	INT, INT, INT, INT, INT, INT, INT, INT
+	DC.L	0				; Exception Vector #48
+	DC.L	0,0,0,0,0,0,0
+	DC.L	0,0,0,0,0,0,0,0
 
 	DC.B	'SEGA MEGA DRIVE '		; Console name (16B)
 	DC.B	'(C)OB1  2022.NOV'		; Copyright notice (16B)
-	DC.B	'T_INT           '
-	DC.B	'                '
+	DC.B	'60 H_INT/S ON A '
+	DC.B	'PAL MEGADRIVE   '
 	DC.B	'                '		; Domestic game name (48B)
-	DC.B	'T_INT           '
-	DC.B	'                '
+	DC.B	'60 H_INT/S ON A '
+	DC.B	'PAL MEGADRIVE   '
 	DC.B	'                '		; Overseas game name (48B)
 	DC.B	'GM'				; Type of product (2B)
 	DC.B	' 00000000-00'			; Product code, version nbr (12B)
@@ -50,148 +47,92 @@ V_INT_RAM	equ	$FFFF00
 	DC.B	'                '
 	DC.B	'                '
 	DC.B	'        '			; Memo (40B)
-	DC.B	'JUE             '		; Country game (16B)
+	DC.B	'E               '		; Country game (16B)
 
-init:
+START
 	include "ICD_BLK4.PRG"
 	
-	move.w	#$8014,GFX_CTRL		; 0 0 0 HINT 0 1 M2 0
-	move.w	#$8164,GFX_CTRL		; 0 DISP VINT DMA V30 1 0 0
-;	move.w	#$8ADF,GFX_CTRL		; H_INT Register
+	MOVE.W	#$8014,GFX_CTRL		; 0 0 0 HINT 0 1 M2 0
+	MOVE.W	#$8164,GFX_CTRL		; 0 DISP VINT DMA V30 1 0 0
 
-	BSR	waitForVBlankSet
-	BSR	waitForVBlankCleared
-	MOVE.B	#1,(clearHInt)
-	MOVE.W	#-1,D6
+	MOVEA.L	#$FF0000,A2
 
-	movea.l	($C0),a0		; Get USP from Exception Vector #48
-	move.l	a0,usp
-	move.w  #$0300,sr		; Set user mode, enable interrupts > 3
-
-	MOVE.W	#$8ADF,GFX_CTRL
-
-	bra	main
-
-
-; *memcpy(void *dest, const void * src, size_t n)
-memcpy:
-	LINK	A6,#0
-	MOVE.L	(8,A6),A1	; A1 = *dest
-	MOVE.L	(12,A6),A0	; A0 = *src
-	MOVE.W	(16,A6),D0	; D0 = n
-	LSR.W	#1,D0
-@loop:
-	MOVE.W	(A0)+,(A1)+
-	DBRA	D0,@loop
-	UNLK	A6
-	RTS
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-waitForVBlankCleared:
-@waitForVBlank:
-	btst	#3,(GFX_CTRL+1)
-	bne.s	@waitForVBlank
-	rts
-
-
-
-waitForVBlankSet:
-@waitForVBlank:
-	btst	#3,(GFX_CTRL+1)
-	beq.s	@waitForVBlank
-	rts
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	MOVEA.L	(48*4),A0		; Get USP from Exception Vector #48
+	MOVE.L	A0,USP
+	MOVE.W  #$0300,SR		; Set user mode, enable interrupts > 3
 
 
 
 ; =============================================================================
-main:
-	addq.w	#1,(tTimer)
-	;move.w	sr,d3
+MAIN
+	BRA	MAIN
 
 
-	BRA	main
+; PAL   0      20      40      60      80      100
+; ------+-------+-------+-------+-------+-------+---------------------------->
+;
+; ------+-----+------+------+------+------+-----+---------------------------->
+; NTSC  0     16    33      50     66     83   100
+;
+; Line
+; nbr   0    223    215    161    107    54   222
+; PAL224
+;
+; If we generate an H_INT on these lines, we get an almost regular 60fps
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+; On each V_INT, we modify reg #10.
+; * On first frame, reg #10 = 223. So we generate an H_INT each 223th line,
+; that is 223, 446, 669, ... and so on. This isn't a problem since this is
+; cleared ; as soon as line 224 with V_INT.
+; * On second frame, we set reg #10 = 215. So we generate an H_INT each 215th
+; line, that is 215, 430, 645, ... and so on. This isn't a problem since this
+; is cleared as soon as line 224 with V_INT.
+; * On third frame, we set reg #10 = 161. So we generate an H_INT each 161th
+; line, that is 161, 483, ... and so on. This isn't a problem since this
+; is cleared as soon as line 224 with V_INT.
+; * On fourth frame, we set reg #10 = 107. So we generate an H_INT each 107th
+; line, that is 107, and 214. This is a problem because we do NOT want an H_INT
+; on 214th line. Changing reg #10 on line 107 is useless since this register
+; will only be read on next H_INT, that is 214. Thus, we can't prevent the next
+; H_INT to be on line 214. So, in this case, we mute the next H_INT.
+; * On fifth frame, we set reg #10 = 54. So we generate an H_INT each 54th
+; line, that is 54, 108, 162, ... and so on. This is a problem because we do
+; NOT want an H_INT on these next lines ; but we DO want an H_INT on line 222.
+; So, we set reg #10 to 72h = 114, and we mute the next H_INT. Then, when the
+; next H_INT happens on line 108, reg #10 will be read and asked to raise H_INT
+; on line 108 + 114 = 222. That's the sixth H_INT in our 5 PAL 224 frames.
 
 ; =============================================================================
 INT:
-	move.w	#$2700,SR		; Disable Interrupts
-@infLoop:
-	bra.s	@infLoop
+	MOVE.W	#$2700,SR		; Disable Interrupts
+@INF_LOOP:
+	BRA.S	@INF_LOOP
+
+
+
+
 
 H_INT:
-	MOVE.B	(clearHInt),d0
-	TST.B	D0
-	BEQ.S	@noClear
-	MOVE.B	#0,(clearHInt)
-	BRA.S	@epilog
-@noClear:
-	addq.w	#1,d4
-	moveq	#0,d0
-	move.b	(freq_index2),d0
-	lea	@freq2(pc),a1
-	move.b	(a1,d0),d1
-	andi.w	#$FF,d1
-	addi.w	#$8A00,d1
-	move.w	d1,GFX_CTRL
-	addq.b	#1,d0
-	cmpi.b	#6,d0
-	blt	@skip2
-	moveq	#0,d0
-@skip2:
-	move.b	d0,(freq_index2)
-@epilog:
-	rte
-@freq2:
-	DC.B	$FF,$FF,$FF,$FF,$00,$72
-	even
+	TST.B	(muteHInt)
+	BEQ.S	@DO
+	CLR.B	(muteHInt)
+	RTE
+@DO
+
+	MOVE.L	D0,-(SP)
+
+	ADDQ.W	#1,(hCnt)
+
+	MOVE.W	(HV_COUNTER),D0
+	LSR.W	#8,D0
+	CMPI.W	#$70,D0
+	BGE.S	@NO_STUTTER
+	MOVE.B	#1,(muteHInt)
+@NO_STUTTER
+	MOVE.W	#$8A72,GFX_CTRL
+
+	MOVE.L	(SP)+,D0
+	RTE
 H_INT_END:
 
 
@@ -199,30 +140,29 @@ H_INT_END:
 
 
 V_INT:
-	MOVE.B	(clearVInt),d0
-	CMPI.B	#0,D0
-	BEQ.S	@noClear
-	MOVE.B	#0,(clearVInt)
-	BRA.S	@epilog
-@noClear:
-	addq.w	#1,d6
-	moveq	#0,d0
-	move.b	(freq_index),d0
-	lea	@freq,a1
-	move.b	(a1,d0),d1
-	andi.w	#$FF,d1
-	addi.w	#$8A00,d1
-	move.w	d1,GFX_CTRL
-	addq.w	#1,d0
-	cmpi.w	#5,d0
-	blt	@skip
-	moveq	#0,d0
-@skip:
-	move.b	d0,(freq_index)
-@epilog
-	rte
-@freq:
-	DC.B	223,215,161,107,54
+	MOVEM.L	A0/D0-D1,-(SP)
+	ADDQ.W	#1,(vCnt)
+
+	LEA	@TABLE,A0
+	MOVEQ	#0,D0
+	MOVE.B	(INDEX),D0
+	MOVEQ	#0,D1
+	MOVE.B	(A0,D0),D1
+
+	ANDI.W	#$FF,D1
+	ORI.W	#$8A00,D1
+	MOVE.W	D1,GFX_CTRL
+
+	ADDQ	#1,D0
+	CMPI.B	#5,D0
+	BLT.S	@NO_OVERFLOW
+	MOVEQ	#0,D0
+@NO_OVERFLOW
+	MOVE.B	D0,(INDEX)
+
+	MOVEM.L	(SP)+,A0/D0-D1
+	RTE
+@TABLE	DC.B	$DF,$D7,$A1,$6B,$36
 	even
 V_INT_END:
 
